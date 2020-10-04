60123003fb8d705d84fbd04ee420e509b8b5ac04
Jhonah <buryakone@gmail.com>
2020-10-04 18:32:21 +0700

    удаленил и переписал заново как умел логику формы, на timeOut застопорился взял из оригинала

--- js/app.js
+++ js/app.js
@@ -1,28 +1,55 @@
+const form = document.querySelector('#message-form');
+const message = document.querySelector('#message');
+const content = document.querySelector('.message-content');
+const feedback = document.querySelector('.feedback');
+const key = 'message-content';
 
-(function() {
-//Select the input element
-const form = document.querySelector('#message-form')
-//Set up Submit Button
-form.addEventListener('submit', function(e){
-    // prevent the form's default submission action
-    e.preventDefault()
-    //Get user's input from from
-    const message = document.querySelector('#message')
-    const feedback = document.querySelector('.feedback')
-    const messageContent = document.querySelector('.message-content')
+/* loading value */
+//content.textContent = getSavedValue(key);
 
-    if (message.value === ''){
-        feedback.classList.add('show')
-        setTimeout(function(){
-        feedback.classList.remove('show')
-        }, 2000)
+form.addEventListener('submit', function (event) {
+
+    /* prevent the form's default submission action
+       Заменило мне localStorage, но оно умело
+       после обновления страницы сохранять значение,
+       prevent просто предотвращает обновление
+       после нажатия кнопки submit */
+    event.preventDefault();
+
+    if (message.value === '') {
+        feedback.classList.add('show');
+        //console.log(feedback.classList); // показывает существующие классы по селектору feedback
+        setTimeout(() => {
+            feedback.classList.remove('show')
+        }, 2000);
+        //displayVisible();
     } else {
-        //Change message content and clear the message input
-        messageContent.textContent = message.value
-        message.value = ''
+        content.textContent = message.value;
+        localStorage.setItem(key, message.value);
+        message.value = '';
     }
 })
-})()
 
+function getSavedValue (value) {
+    if (!localStorage.getItem(value)) {
+        return "nothing";
+    }
+    return localStorage.getItem(value);
+}
 
+/* Пример предотвращения явного события */
+function stopDefAction(event) {
+    event.preventDefault();
+}
 
+document.getElementById('my-checkbox').addEventListener('click', stopDefAction, false);
+
+/*function displayVisible() {
+    let delay = 100;
+    setTimeout(function run() {
+        feedback.style.display = "block";
+        setTimeout(run, delay);
+    }, delay);
+}*/
+
+//alert(getSavedValue(key));
